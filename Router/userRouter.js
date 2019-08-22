e970eadc66f2d90604e059a7499d7f89f50462ef
Zolton <gmzesq@gmail.com>
2019-08-22 16:02:25 -0400

    Server and router basic GET requests work, pass tests

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const user = require("./routerHelpers")
 
 router.get("/", (req, res)=>{
     res.status(200).json({Hello: "from userRouter.js"})
+})
+
+router.get("/users", (req, res)=>{
+    user.getAll().then(post=>{
+        res.status(200).json(post)
+    })
+    .catch(error=>{
+        res.status(500).json(error)
+    })
+    
 })
 
 module.exports = router
--- server.spec.js
+++ server.spec.js
@@ -1,18 +1,29 @@
-const db = require("./data/db-Config")
-const router = require("./Router/userRouter")
-const server = require("./server")
-const request = require("supertest")
+const db = require("./data/db-Config");
+const router = require("./Router/userRouter");
+const server = require("./server");
+const request = require("supertest");
 
-describe("GET requests", ()=>{
-    it("get server.js", ()=>{
-        return request(server).get("/").then(res=>{
-            expect(res.status).toBe(200)
-        })
-    })
-    it("get router.js", ()=>{
-        return request(router).get("/api").then(res=>{
-            expect(res.status).toBe(200)
-        })
-    })
-
-})
+describe("GET requests", () => {
+  it("get server.js", () => {
+    return request(server)
+      .get("/")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("get router.js", () => {
+    return request(server)
+      .get("/api")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("get all users", () => {
+    return request(server)
+      .get("/api/users")
+      .then(res => {
+        expect(res.status).toBe(200)
+        expect(res.body).toBe("array")
+      });
+  });
+});
